ac34648c2f0c46df2746d3de96cb26b28ca08ac7
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-08-17 20:37:19 -0300

    Config EFCore

--- PSIUWeb/Data/AppDbContext.cs
+++ PSIUWeb/Data/AppDbContext.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PSIUWeb.Models;
 
 namespace PSIUWeb.Data
 {
-    public class AppDbContext
+    public class AppDbContext : 
+        IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        public AppDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Pacient>? Pacients { get; set; }
+
     }
 }
--- PSIUWeb/Program.cs
+++ PSIUWeb/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PSIUWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer( 
+        builder.Configuration.
+            GetConnectionString("PsiuContext") 
+    ) 
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
